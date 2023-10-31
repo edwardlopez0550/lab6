735f33047159de51ea9f8c1a08a632ab63bc36d5
Eddie <edwardlopez@ufl.edu>
2023-10-31 03:01:50 -0400

    commit 1

---
+++ scratches/encoder.py
@@ -0,0 +1,41 @@
+def menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    global option
+    option = input("Please enter an option: ")
+
+def encode(password):
+    global enc_pass
+    enc_pass = ""
+    for i in range(0, len(password)):
+        if int(password[i]) < 7:
+            num = int(password[i]) + 4
+            enc_pass += str(num)
+        else:
+            num = int(password[i]) - 9
+            enc_pass += str(num)
+    print(enc_pass)
+
+
+
+menu()
+while True:
+    if option == "1":
+        password = input("Please input your passcode: ")
+        encode(password)
+        menu()
+        continue
+    elif option == "2":
+        decode(enc_pass)
+        menu()
+        continue
+    elif option == "3":
+        break
+    else:
+        print("Invalid option")
+        menu()
+
+
